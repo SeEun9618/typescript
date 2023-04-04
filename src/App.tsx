5b660c3c15a3d86fedef206f10cc40f550e25b26
SeEun9618 <seeun9618@gmail.com>
2023-04-04 22:13:30 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Greetings from "./Greetings";
+import Counter from "./Counter";
+import MyForm from "./MyForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Greetings.tsx
+// const App: React.FC = () => {
+//
+//   const onClick = (name: string) => {
+//     console.log(`${name} 안녕!!~`);
+//   };
+//
+//   return <Greetings name="Seeun" onClick={onClick}/>;
+// }
+
+
+//Counter.tsx
+// const App: React.FC = () => {
+//   return <Counter />;
+// }
+
+//MyForm.tsx
+const App: React.FC = () => {
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+
+  return <MyForm onSubmit={onSubmit} />;
 }
 
 export default App;
